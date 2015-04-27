5c0c29fcde9909ef5b85c3a6d2a477a4b57b46d9
Scott King <kingsc850@gmail.com>
2015-04-27 13:50:23 -0400

    Update readme

--- hack.exe.c
+++ hack.exe.c
@@ -37,7 +37,7 @@
 #define GETIPV4         1             // Resolve ipv4? (0 = none, >0 = max)
 #define SPOOPYASCII     printf("\
           .                                                      .\n\
-	.n                   .                 .                  n.\n\
+	      .n                   .                 .                  n.\n\
   .   .dP                  dP                   9b                 9b.    .\n\
  4    qXb         .       dX                     Xb       .        dXp     t\n\
 dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb\n\
@@ -45,18 +45,18 @@ dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb\n\
  9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP\n\
   `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'\n\
     `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'\n\
-	~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~\n\
-			)b.  .dbo.dP'`v'`9b.odb.  .dX(\n\
-		      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.\n\
-		     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb\n\
-		    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb\n\
-		    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP\n\
-		     `'      9XXXXXX(   )XXXXXXP      `'\n\
-			      XXXX X.`v'.X XXXX\n\
-			      XP^X'`b   d'`X^XX\n\
-			      X. 9  `   '  P )X\n\
-			      `b  `       '  d'\n\
-			       `             '\n\n\
+	      ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~\n\
+			                  )b.  .dbo.dP'`v'`9b.odb.  .dX(\n\
+								      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.\n\
+								     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb\n\
+								    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb\n\
+								    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP\n\
+								     `'      9XXXXXX(   )XXXXXXP      `'\n\
+												      XXXX X.`v'.X XXXX\n\
+												      XP^X'`b   d'`X^XX\n\
+												      X. 9  `   '  P )X\n\
+												      `b  `       '  d'\n\
+												       `             '\n\n\
 ");
 
 pthread_t thread;
