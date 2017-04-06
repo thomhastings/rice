5cdf0c15cbf9f210ed27b172a4ecb0eee862fc65
thomhastings <thom@attackvector.org>
2017-04-06 09:45:46 -0500

    get-figlet get-buuf

---
+++ get-figlet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Get Figlet script"
+echo "(c) Thom Hastings 2011 New BSD license"
+echo
+echo "Installing Figlet..."
+sudo apt install -y figlet
+echo
+echo "Getting Figlet fonts..."
+cd /usr/share/figlet
+sudo wget -r -l1 -H -t1 -nd -N -np -A.flf -erobots=off http://www.textfiles.com/art/
+cd ~
+figlet -f graffiti "\"Welcome to the real world.\""
+echo "                 - Morpheus, The Matrix"
