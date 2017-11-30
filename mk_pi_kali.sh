a6be9cc8b911d169089958c2d34174602793ede8
thomhastings <thom@attackvector.org>
2017-11-30 08:01:50 -0600

    add kali pi

--- mk_pi_kali.sh
+++ mk_pi_kali.sh
@@ -2,6 +2,7 @@
 # Thom Hastings 2017
 # (c) NEW BSD LICENSE
 # make pi kali .shell
+# source: https://docs.kali.org/downloading/kali-linux-live-usb-install
 diskutil list
 diskutil unmountDisk /dev/disk2
 sudo dd if=kali-2017.3-rpi3-nexmon.img of=/dev/disk2 bs=1m
