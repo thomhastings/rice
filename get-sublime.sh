e012d38cd147a60a4d6be8182ccda2f4526d44f8
thomhastings <thom@attackvector.org>
2018-01-22 21:08:35 -0800

    add get-virtualbox

--- get-sublime.sh
+++ get-sublime.sh
@@ -5,7 +5,7 @@
 # https://www.sublimetext.com/docs/3/linux_repositories.html#apt
 #
 # install prerequisites
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 # wget and install GPG key
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 # we do not have a license so we cannot choose the dev channel over stable
@@ -13,4 +13,4 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 # apt-get would be less risky
 sudo apt update
 # finally, install sublime!
-sudo apt install sublime-text
+sudo apt install -y sublime-text
