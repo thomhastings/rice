5c0f0197c7c19019bec64825def0d11b11f96b10
Thom Hastings <thom@attackvector.org>
2018-08-30 15:57:39 -0700

    bugfix

--- get-wallpaper.sh
+++ get-wallpaper.sh
@@ -13,7 +13,7 @@ cd ~/Pictures
 wget https://orig00.deviantart.net/b01d/f/2017/363/6/1/isometric_kali_dragon_by_xxdigipxx-dbyaw60.png
 wget https://orig00.deviantart.net/0ba1/f/2017/363/3/5/isometric_kali_dragon_lockscreen_by_xxdigipxx-dbyaw8g.png
 echo "Setting GNOME Wallpaper..."
-gsettings set org.gnome.desktop.background picture-uri file:////~/Pictures/isometric_kali_dragon_by_xxdigipxx-dbyaw60.png
+gsettings set org.gnome.desktop.background picture-uri file:////root/Pictures/isometric_kali_dragon_by_xxdigipxx-dbyaw60.png
 echo "Setting GNOME Lock Screen..."
-gsettings set org.gnome.desktop.screensaver picture-uri file:////~/Pictures/isometric_kali_dragon_lockscreen_by_xxdigipxx-dbyaw8g.png
+gsettings set org.gnome.desktop.screensaver picture-uri file:////root/Pictures/isometric_kali_dragon_lockscreen_by_xxdigipxx-dbyaw8g.png
 echo "Done!"
