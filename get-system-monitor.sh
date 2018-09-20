c05dad6a0fa742e60a3e6f46361478a995bddd46
Thom Hastings <thom@attackvector.org>
2018-09-20 13:31:06 -0700

    add get-system-monitor.sh

---
+++ get-system-monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Install system-monitor GNOME Shell extension script
+# (c) Thom Hastings 2018 - New BSD License
+sudo apt-get install gnome-shell-extension-system-monitor
+gnome-shell-extension-tool --enable-extension=system-monitor@paradoxxx.zero.gmail.com
