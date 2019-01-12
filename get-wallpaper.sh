7eb38d7f4bcac49ccc1df7e8e91404c7a431670a
thomhastings <thom@attackvector.org>
2019-01-11 23:07:14 -0800

    tears

--- get-wallpaper.sh
+++ get-wallpaper.sh
@@ -1,1 +1,5 @@
-wget https://wiki.evageeks.org/images/a/a0/M25_C655_big.jpg
+#!/bin/sh
+cd ~/Pictures
+wget -c https://wiki.evageeks.org/images/a/a0/M25_C655_big.jpg
+sh wgetall.sh jpg http://www.jkhp.it/OS-tan/desktops.htm
+sudo cp *.jpg /usr/share/backgrounds/
--- wgetall.sh
+++ wgetall.sh
@@ -5,7 +5,8 @@
 # TODO: allow multiple extension arguments (e.g. mp3,wav,flac)
 if [[ "$1" == "" ]] ; then
   echo "Usage: ./wgetall.sh [EXT] [URL]     -- wgets all files of extension EXT from base URL."
+  echo "e.g: ./wgetall.sh mp3 https://bootiemashup.com/best-of-bootie/"
   exit
 fi
-wget -r -l1 -H -t1 -nd -N -np -A.$1 -erobots=off "$2"
+wget -c -r -l1 -H -t1 -nd -N -np -A.$1 -erobots=off "$2"
 echo "w-got all .$1 from $2"
