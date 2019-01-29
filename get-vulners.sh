1ecead8ab489b4237cd152c5dc62e8d66a13f04f
Thom Hastings <tghastings@gmail.com>
2019-01-29 05:29:18 -0800

    Do you even lift? Get Buuf!

--- get-vulners.sh
+++ get-vulners.sh
@@ -3,5 +3,6 @@
 # (c) Thom Hastings 2018 GNU GPLv3
 #
 # wget it to the right directory
+# TODO: include VULSCAN.NSE: https://www.computec.ch/projekte/vulscan/?s=download
 cd /usr/share/nmap/scripts
 wget https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
