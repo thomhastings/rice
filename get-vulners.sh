d23b81a3186d35ea961376b557305c92f860645b
thomhastings <thom@attackvector.org>
2019-02-07 18:53:46 -0800

    reamde upd8

--- get-vulners.sh
+++ get-vulners.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # download & install nmap-vulners
 # (c) Thom Hastings 2018 GNU GPLv3
-#
-# wget it to the right directory
-# TODO: include VULSCAN.NSE: https://www.computec.ch/projekte/vulscan/?s=download
+# Update 2019 w/ moar firepower!!! AGPL
+# just wget the txt to the right directory
+# NEW! includes VULSCAN.NSE: https://www.computec.ch/projekte/vulscan/?s=download
 cd /usr/share/nmap/scripts
 wget https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
+mkdir vulscan
+cd vulscan
+git clone https://github.com/scipag/vulscan.git
