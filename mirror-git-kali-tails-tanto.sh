3b03193087bcf7de92029ed648c39167cfa8c0fb
Needle <user@tanto.li>
2019-02-12 19:58:39 -0800

    fix

--- mirror-git-kali-tails-tanto.sh
+++ mirror-git-kali-tails-tanto.sh
@@ -1,6 +1,8 @@
 echo "Mirroring Tails' git index..."
 git clone https://git-tails.immerda.ch/tails
+input "CTRL+C now before it tries to mirror all of Kali's repos."
 mkdir kali
+cd kali
 echo "Mirroring all Kali packages via git..."
 for LINE in `curl http://git.kali.org/gitweb/?a=project_index | sort`
 do
