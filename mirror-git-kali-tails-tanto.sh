526d89db1ac082a68d98a229d690c79afe72e349
Needle <user@tanto.li>
2019-02-12 20:22:38 -0800

    unfubar'd

--- mirror-git-kali-tails-tanto.sh
+++ mirror-git-kali-tails-tanto.sh
@@ -1,12 +1,32 @@
 echo "Mirroring Tails' git index..."
 git clone https://git-tails.immerda.ch/tails
-input "CTRL+C now before it tries to mirror all of Kali's repos."
+echo "Attempting to update tails..."
+cd tails
+git pull
+cd ..
+echo "Moving on to Kali..." && sleep 1
+echo "CTRL+C now before it tries to mirror all of Kali's repos."
+sleep 1
+echo "9..."
+sleep 1
+echo "8..."
+sleep 1
+echo "7..."
+sleep 1
+echo "6..."
+sleep 1
+echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..." && sleep 1 && echo "2..." && sleep 1 && echo "1..."
+sleep 1
 mkdir kali
+echo "Mirroring or updating all Kali packages via git..."
 cd kali
-echo "Mirroring all Kali packages via git..."
 for LINE in `curl http://git.kali.org/gitweb/?a=project_index | sort`
 do
-	if [ ! -e "tails/kali/`echo $LINE | awk -F. '{print $1}'`" ]
+	if [ ! -e "kali/`echo $LINE | awk -F. '{print $1}'`" ]
 	then
 		git clone git://git.kali.org/$LINE
 	else
