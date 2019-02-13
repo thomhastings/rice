8df720c6c96a2806737dc5e0d5431b5959a91571
Thom Hastings <tghastings@gmail.com>
2019-02-12 22:02:28 -0800

    !

--- buufify.sh
+++ buufify.sh
@@ -4,6 +4,7 @@
 # Buufify my Qubes-OS:)
 # a post-install script
 # by ThomG.Ha5tingerVII
+# actually this only downloads a wallpaper, that's it
 #
 ###########
 # New BSD 3-Clause
@@ -15,4 +16,4 @@ cd /usr/share/backgrounds
 sudo wget -c -O wallpaper.jpg https://pix.toile-libre.org/upload/original/1253647589.jpg
 #mv wallpaper.jpg /usr/share/backgrounds
 cp wallpaper.jpg ~/Pictures
-
+# Run get-buuf.sh next to set GNOME icons
--- get-wallpaper.sh
+++ get-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/Pictures
-curl http://wiki.evageeks.org/images/a/a0/M25_C655_big.jpg > wallpaper.jpg
+curl http://wiki.evageeks.org/images/a/a0/M25_C655_big.jpg > eva_longinus.jpg
 wget -c -r -l1 -H -t1 -nd -N -np -A.jpg -erobots=off "http://www.jkhp.it/OS-tan/desktops.htm"
 if [ ! -e "/usr/share/backgronds/" ]
 then
