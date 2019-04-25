e4a0b906b8d62e59ad4e4b7d89901e1fefc1cc6f
thomhastings <thom@attackvector.org>
2019-04-25 01:50:57 -0700

    better

--- new.sh
+++ new.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo
+echo "Using git"
 which git
 git init
 git submodule add https://github.com/thomhastings/bt5-scripts
+git submodule add https://github.com/derv82/wifite2
 git submodule add https://github.com/thomhastings/tanto
+git submodule add https://github.com/kanedasan/knife
 git submodule add https://github.com/ksoona/tanto ktanto
 git submodule add https://github.com/thomhastings/rice
 git submodule add https://github.com/lionsec/katoolin
 git submodule add https://github.com/brainfucksec/kalitorify
 git submodule add https://github.com/trustedsec/ptf
-git submadule add https://git-tails.immerda.ch/tails
+git submodule add https://git-tails.immerda.ch/tails
