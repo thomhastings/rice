9a503673165c04ad33fd18aba374fe01f8a46b42
thomhastings <thom@attackvector.org>
2019-04-25 04:06:13 -0700

    borked, needfix

--- new.sh
+++ new.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
-echo
+echo ""
 echo "Using git"
 which git
+echo ""
+echo "Initializing..."
 git init
+echo ""
+echo "Status:"
 git status
-git submodule add -f https://github.com/thomhastings/bt5-scripts
-git submodule add -f https://github.com/derv82/wifite2
+echo "Collecting sources..."
+git submodule add -f https://git-tails.immerda.ch/tails tails-src
+echo ""
+if [ ! -e 'kali' ]
+	echo "no kali dir detected, ergo.."
+	mkdir kali
+echo ""
+echo "Adding my faves directly..."
+git submodule add -f https://github.com/BrainFuckSec/kalitorify kali/kalitorify
+git submodule add -f https://github.com/TrustedSec/ptf kali/ptf
+git submodule add -f https://github.com/LionSec/katoolin kali/katoolin
+git submodule add -f https://github.com/derv82/wifite2 kali/wifite2
+git submoudle add -f https://github.com/kaneda/knife
+echo ""
+echo "All looks good from here, Knight Rider."
+echo ""
+echo "Going for Kali..."
+for LINE in `curl http://git.kali.org/gitweb/?a=project_index | sort | cut -d " " -f 1`
+	if [ ! -e "kali/`echo $LINE | awk -F. '{print $1}'`" ]
+		git submodule add -f git://git.kali.org/$LINE
+echo ""
+echo "Great success!"
+echo ""
+echo "Finally, my piece of the resistance..."
+git submodule add -f https://github.com/thomhastings/bt5-scripts kali/thomhastings/bt5-scripts
 git submodule add -f https://github.com/thomhastings/tanto
-git submodule add -f https://github.com/kaneda/knife
 git submodule add -f https://github.com/ksoona/tanto ktanto
 git submodule add -f https://github.com/thomhastings/rice
-git submodule add -f https://github.com/lionsec/katoolin
-git submodule add -f https://github.com/brainfucksec/kalitorify
-git submodule add -f https://github.com/trustedsec/ptf
-git submodule add -f https://git-tails.immerda.ch/tails
 git status
 git add *
 git status
-git commit -a -m "source submodules added successfully."
+git commit -a -m "Update script success, so push."
 git status
 git push -u
