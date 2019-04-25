8c93882820d0dde362f4f6fbab11c6017b342c22
thomhastings <thom@attackvector.org>
2019-04-25 01:43:51 -0700

    playing

---
+++ new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+which git
+git init
+git submodule add https://github.com/thomhastings/bt5-scripts
+git submodule add https://github.com/thomhastings/tanto
+git submodule add https://github.com/ksoona/tanto ktanto
+git submodule add https://github.com/thomhastings/rice
+git submodule add https://github.com/lionsec/katoolin
+git submodule add https://github.com/brainfucksec/kalitorify
+git submodule add https://github.com/trustedsec/ptf
+git submadule add https://git-tails.immerda.ch/tails
