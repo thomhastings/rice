851c41a6d9f564f48ed3f7e8da211b9534052b65
thomhastings <thom@attackvector.org>
2019-04-25 01:53:16 -0700

    better

--- new.sh
+++ new.sh
@@ -3,13 +3,20 @@ echo
 echo "Using git"
 which git
 git init
-git submodule add https://github.com/thomhastings/bt5-scripts
-git submodule add https://github.com/derv82/wifite2
-git submodule add https://github.com/thomhastings/tanto
-git submodule add https://github.com/kanedasan/knife
-git submodule add https://github.com/ksoona/tanto ktanto
-git submodule add https://github.com/thomhastings/rice
-git submodule add https://github.com/lionsec/katoolin
-git submodule add https://github.com/brainfucksec/kalitorify
-git submodule add https://github.com/trustedsec/ptf
-git submodule add https://git-tails.immerda.ch/tails
+git status
+git submodule add -f https://github.com/thomhastings/bt5-scripts
+git submodule add -f https://github.com/derv82/wifite2
+git submodule add -f https://github.com/thomhastings/tanto
+git submodule add -f https://github.com/kanedasan/knife
+git submodule add -f https://github.com/ksoona/tanto ktanto
+git submodule add -f https://github.com/thomhastings/rice
+git submodule add -f https://github.com/lionsec/katoolin
+git submodule add -f https://github.com/brainfucksec/kalitorify
+git submodule add -f https://github.com/trustedsec/ptf
+git submodule add -f https://git-tails.immerda.ch/tails
+git status
+git add *
+git status
+git commit -a -m "source submodules added successfully."
+git status
+git push -u
