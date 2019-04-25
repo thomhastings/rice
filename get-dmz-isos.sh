f5852f8cc784112050a6fd70b0cbc081fca0c5a9
thomhastings <thom@attackvector.org>
2019-04-25 03:52:23 -0700

    source submodules added successfully.

--- get-dmz-isos.sh
+++ get-dmz-isos.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-git clone https://github.com/LionSec/katoolin
 #I been bash'n away in the Bourne Again Shell!
 # mac? brew install wget
 # deb? apt install wget
-#wget -c https://github.com/dtag-dev-sec/tpotce/releases/download/18.11/tpot.iso -O tpot-multi-honeypot.iso
+wget -c https://github.com/dtag-dev-sec/tpotce/releases/download/18.11/tpot.iso -O tpot-multi-honeypot.iso
 #tpot.iso	https://github.com/dtag-dev-sec/tpotce
-git clone https://github.com/dtag-dev-sec/tpotce &
+#git clone https://github.com/dtag-dev-sec/tpotce &
 #firefox https://n0where.net/multi-honeypot-platform &
 #Lab26.zip	https://www.vulnhub.com/entry/lab26-11,190/
 #wget -c https://lab26.net/static/files/Lab26.zip -O Lab26_1.1WebApp-CyberFiringRange.zip
@@ -15,18 +14,17 @@ git clone https://github.com/dtag-dev-sec/tpotce &
 # Greenbone Security Assistant is a dashboard (EU:CE=CommunityEdition) (Greenbone Ltd.)
 #firefox https://www.greenbone.net/en/install_use_gce/ &
 #sleep 9
-#wget -c https://dl.greenbone.net/download/VM/gsm-ce-4.2.24.iso
+wget -c https://dl.greenbone.net/download/VM/gsm-ce-4.2.24.iso
 #echo "Opening Greenbone Forums https://community.greenbone.net"
 #firefox https://community.greenbone.net/ &
 #echo "Yet Another Shameless Plug for Greenbone:"
 #firefox https://www.greenbone.net/en/community-edition/ &
 #######################################################################################
 #securityonion*.iso							securityonion.net
-#wget -c https://github.com/Security-Onion-Solutions/security-onion/releases/download/v16.04.5.5_20181212/securityonion-16.04.5.5.iso
+wget -c https://github.com/Security-Onion-Solutions/security-onion/releases/download/v16.04.5.5_20181212/securityonion-16.04.5.5.iso
 # Security Onion is a virtual firewall "appliance" (contains Suricata, Sagan, and Snort.)
 # N.B: Should also be deployed on either end of the DMZ, for proper Zone Segmentation
 #####################################################################################
-git clone https://github.com/derv82/wifite2 &
 #cd wifite2
 #sudo python Wifite2.py
 # Wifite.py is the best way to Crack WiFi short of the KRACK attack: krackattacks.com
@@ -36,15 +34,14 @@ git clone https://github.com/derv82/wifite2 &
 # "Are we still doing phrasing? Because if we stopped doing phrasing and nobody told"
 #####################################################################################
 #kali-*.iso								kali.org
-#wget -c cdimage.kali.org/kali-2018.4/kali-linux-light-2018.4-amd64.iso
+wget -c cdimage.kali.org/kali-2018.4/kali-linux-light-2018.4-amd64.iso
 #firefox https://github.com/Security-Onion-Solutions/security-onion/blob/master/Verify_ISO.md#160455-iso-image-built-on-20181212 &
 # Previously BackTrack, a distro based on Auditor, Whax... & WifiSlax/Knoppix...
 #             🙏MUTS🙇                    remote-exploit.org;backtrack-linux.org
 # Penetration testing "swiss-army knife" w/ over 500 tools... incl. wifite.py
-git clone https://github.com/brainfucksec/kalitorify &
 #############################################################################
 #tails-*.iso								tails.boum.org
-#wget -c https://tails.as1101.net/tails/stable/tails-amd64-3.11/tails-amd64-3.11.iso &
+wget -c https://mirrors.edge.kernel.org/tails/stable/tails-amd64-3.13.1/tails-amd64-3.13.1.iso
 #firefox https://tails.boum.org/install/download/index.en.html &
 # Privacy & Anonymity focused distro, runs Tor, wipes RAM after shutdown, etc.
 ##############################################################################
@@ -56,5 +53,5 @@ git clone https://github.com/brainfucksec/kalitorify &
 #################################################################################
 # Just try to remember it's CTRL+SHIFT+V and CTRL+SHIFT+P to Cut & Paste vm-to-vm
 #################################################################################
-#wget -c https://mirrors.edge.kernel.org/qubes/iso/Qubes-R4.0.1-x86_64.iso
+wget -c https://mirrors.edge.kernel.org/qubes/iso/Qubes-R4.0.1-x86_64.iso
 echo "Done!"
